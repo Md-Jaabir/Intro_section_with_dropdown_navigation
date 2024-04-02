7bd99c203ac7f8e099d5f126899dc40686ef1f4d
Md-Jaabir <100750999+Md-Jaabir@users.noreply.github.com>
2024-04-02 11:48:14 +0600

    second commit

--- script.js
+++ script.js
@@ -21,3 +21,37 @@ document.querySelectorAll("a.dropdown .dropdown-list").forEach(list=>{
         list.style.display="none";
     });
 });
+
+document.querySelectorAll(".dropdown-list li").forEach(item=>{
+    item.addEventListener("click",(event)=>{
+        item.parentNode.style.display="none";
+    });
+});
+
+function openMenu(){
+    document.querySelector(".mobile-menu").classList.add("show");
+}
+
+function closeMenu(){
+    document.querySelector(".mobile-menu").classList.remove("show");
+}
+
+function openMobileDropdown1(){
+    if(document.querySelector(".mobile-dropdown.one .dropdown-list").classList.contains("show")){
+        document.querySelector(".mobile-dropdown.one .dropdown-list").classList.remove("show");
+        document.querySelector(".mobile-dropdown.one .header .dropdown").innerHTML=`Features <img src="images/icon-arrow-down.svg" alt="">`;
+    }else{
+        document.querySelector(".mobile-dropdown.one .dropdown-list").classList.add("show");
+        document.querySelector(".mobile-dropdown.one .header .dropdown").innerHTML=`Features <img src="images/icon-arrow-up.svg" alt="">`;
+    }
+}
+
+function openMobileDropdown2(){
+    if(document.querySelector(".mobile-dropdown.two .dropdown-list").classList.contains("show")){
+        document.querySelector(".mobile-dropdown.two .dropdown-list").classList.remove("show");
+        document.querySelector(".mobile-dropdown.two .header .dropdown").innerHTML=`Features <img src="images/icon-arrow-down.svg" alt="">`;
+    }else{
+        document.querySelector(".mobile-dropdown.two .dropdown-list").classList.add("show");
+        document.querySelector(".mobile-dropdown.two .header .dropdown").innerHTML=`Features <img src="images/icon-arrow-up.svg" alt="">`;
+    }
+}
